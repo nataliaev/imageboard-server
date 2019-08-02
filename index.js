94e208044f06aa8f6c2f5c208ee5dbe084174c4a
nataliaev <nataliiaevchenko@gmail.com>
2019-08-02 11:23:17 +0200

    Added Heroku DB

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
+const db = require('./db')
+
 app.listen(port, () => console.log(`Listening on: ${port}`))
