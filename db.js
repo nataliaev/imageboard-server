26b36716c8a62549b441fe93e3478a80d2e4bcfa
nataliaev <nataliiaevchenko@gmail.com>
2019-09-18 10:39:05 +0200

    Add relational DB

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@loca
 
 const db = new Sequelize(databaseUrl)
 
-db.sync()
+db.sync({force: true})
   .then(() => console.log('Database connected'))
   .catch(console.error)
 
--- image/model.js
+++ image/model.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const User = require("../user/model")
 
 const Image = db.define("image", {
   url: Sequelize.STRING,
-  title: Sequelize.STRING,
-  likes: Sequelize.INTEGER
+  title: Sequelize.STRING
 });
+
+Image.belongsTo(User);
+User.hasMany(Image);
+User.belongsTo(Image);
+Image.hasMany(User);
 
 module.exports = Image;
