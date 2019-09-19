ffd618d1b833a98388d625630c33f04415d1483a
nataliaev <nataliiaevchenko@gmail.com>
2019-09-19 15:07:18 +0200

    Add heroku force false

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@loca
 
 const db = new Sequelize(databaseUrl)
 
-db.sync({force: true})
+db.sync({force: false})
   .then(() => console.log('Database connected'))
   .catch(console.error)
 
